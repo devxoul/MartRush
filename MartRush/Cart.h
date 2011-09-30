eba624e392eda8290c02d0ae17ae41f9ce500004
ters213 <ters213@gmail.com>
2011-10-01 02:55:35 +0900

    MC임 카트추가

---
+++ MartRush/Cart.h
@@ -0,0 +1,13 @@
+//
+//  Cart.h
+//  MartRush
+//
+//  Created by sung-eun Im on 11. 10. 1..
+//  Copyright 2011년 Joyfl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Cart : NSObject
+
+@end
---
+++ MartRush/Cart.m
@@ -0,0 +1,23 @@
+//
+//  Cart.m
+//  MartRush
+//
+//  Created by sung-eun Im on 11. 10. 1..
+//  Copyright 2011년 Joyfl. All rights reserved.
+//
+
+#import "Cart.h"
+
+@implementation Cart
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
